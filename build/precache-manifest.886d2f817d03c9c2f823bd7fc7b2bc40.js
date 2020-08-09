81652192c065f52bc7054d426471a38a5f1ee322
Logakrishnan <logakrishnan17@gmail.com>
2020-08-09 15:56:49 +0530

    resolving favicon issue

---
+++ build/precache-manifest.886d2f817d03c9c2f823bd7fc7b2bc40.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eafca8416f6f35e3c9d282eeb4f52a6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e95021babd55a7aa20ee",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fe1c5e0dd710ec5516cd",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e95021babd55a7aa20ee",
+    "url": "/static/js/2.043e5b32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.043e5b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1c5e0dd710ec5516cd",
+    "url": "/static/js/main.16e8277e.chunk.js"
+  },
+  {
+    "revision": "33eed527fe48495980ed",
+    "url": "/static/js/runtime-main.e93d6f6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
